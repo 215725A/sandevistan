ebb1f3fdd0568c3fe9a3233d726fb6a0ba4dcbe6
e215720 <e215720@ie.u-ryukyu.ac.jp>
2023-11-28 13:19:29 +0900

    読み込むで

--- db/init/init.sql
+++ db/init/init.sql
@@ -5,6 +5,28 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS user_info (
+    num VARCHAR(10),
+    langage INT,
+    lecture_num1 INT,
+    lecture_num2 VARCHAR(50),
+    lecuture_name VARCHAR(50),
+    unit VARCHAR(10),
+    week_time VARCHAR(10),
+    must VARCHAR(10),
+    from_ VARCHAR(5),
+    teacher VARCHAR(50),
+    class INT,
+    member INT,
+    scl_year VARCHAR(10),
+    week_day VARCHAR(10),
+    lecture_time VARCHAR(10),
+    remote_ VARCHAR(10),
+    classroom VARCHAR(20),
+    lecture_limit VARCHAR(10),
+    per_code VARCHAR(10),
+    etc VARCHAR(500)
+);
 INSERT INTO users VALUES (
     1,
     'hoge',
@@ -15,3 +37,9 @@ INSERT INTO users VALUES (
     'fuga',
     'fuga@example.com'
 );
+DO $$
+ BEGIN
+     IF (SELECT COUNT(*) FROM user_info) = 0 THEN
+         COPY user_info FROM '/csv/R5_lectures.csv' DELIMITER ',' CSV HEADER;
+     END IF;
+END $$;
---
+++ frontend/src/pages/Readcsv.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+ 
+ function CSVTest() {
+     const [info, setInfo] = useState([]);
+ 
+     useEffect(() => {
+         fetchDB();
+     }, []);
+ 
+     const fetchDB = async () => {
+         try {
+             const response = await fetch('http://localhost:8000/api/csv');
+             if (response.ok) {
+                 const result = await response.json();
+                 setInfo(result);
+             } else {
+                 throw new Error("Failed to fetch data");
+             }
+         } catch(error) {
+             console.error('Error fetching data: ', error);
+         }
+     };
+ 
+     return (
+         <div>
+             <h1>This is CSV test page.</h1>
+ 
+             <table>
+                 <thead>
+                     <tr>
+                         <th>整理番号</th>
+                         <th>シラバス 日英区分</th>
+                         <th>講義コード</th>
+                         <th>科目番号</th>
+                         <th>科目名</th>
+                         <th>単位</th>
+                         <th>週時間</th>
+                         <th>必須選択</th>
+                         <th>所属</th>
+                         <th>担当教員名</th>
+                         <th>組名</th>
+                         <th>登録人員</th>
+                         <th>受講年次</th>
+                         <th>曜日</th>
+                         <th>遠隔授業</th>
+                         <th>教室</th>
+                         <th>期間</th>
+                         <th>許可コード</th>
+                         <th>備考</th>
+
+                     </tr>
+                 </thead>
+                 <tbody>
+                    {info.map(inf => (
+                    <tr>
+                      <td>{inf.num}</td>
+                      <td>{inf.langage}</td>
+                      <td>{inf.lecture_num1}</td>
+                      <td>{inf.lecture_num2}</td>
+                      <td> {inf.lecture_name}</td>
+                      <td>{inf.unit}</td>
+                      <td>{inf.week_time}</td>
+                      <td>{inf.must}</td>
+                      <td>{inf.from_}</td>
+                      <td>{inf.teacher}</td>
+                      <td>{inf.class}</td>
+                      <td>{inf.member}</td>
+                      <td>{inf.scl_year}</td>
+                      <td>{inf.week_day}</td>
+                      <td>{inf.lecture_time}</td>
+                      <td>{inf.remote_}</td>
+                      <td>{inf.classroom}</td>
+                      <td>{inf.lecture_limit}</td>
+                      <td>{inf.per_code}</td>
+                      <td>{inf.etc}</td>
+                    </tr>
+                    ))}
+                 </tbody>
+             </table>
+         </div>
+     );
+ }
+ 
+ export default CSVTest;
